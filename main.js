0ee7aec246f523c162746495cd6b65e084837430
Asma Shahid <asmashahid795@gmail.com>
2024-03-12 01:29:26 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let userName = "asma";
+let password = "1234";
+if (userName == "aasma") {
+    console.log("you are sucsessfully login");
+}
+else if (password == "1234") {
+    console.log("sucsessfully login");
+}
+else {
+    console.log("wrong password");
+}
---
+++ main.ts
@@ -0,0 +1,12 @@
+let userName: string = "asma";
+let password: string = "1234";
+if (userName == "aasma") {
+    console.log("you are sucsessfully login");
+}
+else
+    if (password == "1234") {
+        console.log("sucsessfully login");
+    }
+    else {
+        console.log("wrong password");
+    }
